e29a63663207a1a80e13c2def6e69bb8256bc7b7
Alberto Parravicini <Alberto Parravicini>
2016-06-07 20:19:11 +0200

    Added data preparation pt2

--- src/data preparation.R
+++ src/data preparation.R
@@ -9,8 +9,8 @@ library(nnet) # For binarization
 library(zoo) # For time series
 
 # Import data
-setwd("C:/Users/Storna/Desktop/dm_proj/workspace")
-dataset_polimi <- read.csv("dataset_polimi.csv", stringsAsFactors=FALSE)
+
+dataset_polimi <- read.csv("Original data/dataset_polimi.csv", stringsAsFactors=FALSE)
 
 
 # Extract number from string
@@ -44,44 +44,44 @@ dataset_polimi$Stagione_Mese <- ifelse(dataset_polimi$Mese <= 2 | dataset_polimi
 dataset_polimi$Key <- paste(dataset_polimi$Zona, paste(dataset_polimi$Area, dataset_polimi$Sottoarea, sep="-"), sep="-")
 
 
-# Nota: la settiman è calcolata a partire dal primo gennaio, senza guardare che giorno sia (e.g. settimana inizia dal giovedi per il 2014)
+# Nota: la settiman ? calcolata a partire dal primo gennaio, senza guardare che giorno sia (e.g. settimana inizia dal giovedi per il 2014)
 
 
-# Somma parziale nella settimana
-dataset_polimi$sp_sett <- 0
-for (i in 1:nrow(dataset_polimi)){
-  myProdotto <- dataset_polimi[i,]$Categoria_prodotto
-  myKey <- dataset_polimi[i,]$Key
-  myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
-  mySettimana <- dataset_polimi[i,]$Settimana_Anno
-  myAnno <- dataset_polimi[i,]$Anno
-  temp <- filter(dataset_polimi, Categoria_prodotto==myProdotto, Key==myKey, Settimana_Anno==mySettimana, Anno==myAnno, Giorno_Anno<=myGiorno_Anno)
-  dataset_polimi[i,]$sp_sett <- sum(as.numeric(temp$Vendite))
-}
-
-
-# Somma parziale nel mese
-dataset_polimi$sp_mese<-0
-for (i in 1:nrow(dataset_polimi)){
-  myProdotto <- dataset_polimi[i,]$Categoria_prodotto
-  myKey <- dataset_polimi[i,]$Key
-  myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
-  myMese <- dataset_polimi[i,]$Mese
-  myAnno <- dataset_polimi[i,]$Anno
-  temp <- filter(dataset_polimi, Categoria_prodotto==myProdotto, Key==myKey, Mese==myMese, Anno==myAnno, Giorno_Anno<=myGiorno_Anno)
-  dataset_polimi[i,]$sp_mese <- sum(as.numeric(temp$Vendite))
-}
-
-# Somma parziale nell'anno
-dataset_polimi$sp_anno<-0
-for (i in 1:nrow(dataset_polimi)){
-  myProdotto <- dataset_polimi[i,]$Categoria_prodotto
-  myKey <- dataset_polimi[i,]$Key
-  myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
-  myAnno <- dataset_polimi[i,]$Anno
-  temp <- filter(dataset_polimi, Categoria_prodotto==myProdotto, Key==myKey, Anno==myAnno, Giorno_Anno<=myGiorno_Anno)
-  dataset_polimi[i,]$sp_anno <- sum(as.numeric(temp$Vendite))
-}
+# # Somma parziale nella settimana
+# dataset_polimi$sp_sett <- 0
+# for (i in 1:nrow(dataset_polimi)){
+#   myProdotto <- dataset_polimi[i,]$Categoria_prodotto
+#   myKey <- dataset_polimi[i,]$Key
+#   myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
+#   mySettimana <- dataset_polimi[i,]$Settimana_Anno
+#   myAnno <- dataset_polimi[i,]$Anno
+#   temp <- filter(dataset_polimi, Categoria_prodotto==myProdotto, Key==myKey, Settimana_Anno==mySettimana, Anno==myAnno, Giorno_Anno<=myGiorno_Anno)
+#   dataset_polimi[i,]$sp_sett <- sum(as.numeric(temp$Vendite))
+# }
+# 
+# 
+# # Somma parziale nel mese
+# dataset_polimi$sp_mese<-0
+# for (i in 1:nrow(dataset_polimi)){
+#   myProdotto <- dataset_polimi[i,]$Categoria_prodotto
+#   myKey <- dataset_polimi[i,]$Key
+#   myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
+#   myMese <- dataset_polimi[i,]$Mese
+#   myAnno <- dataset_polimi[i,]$Anno
+#   temp <- filter(dataset_polimi, Categoria_prodotto==myProdotto, Key==myKey, Mese==myMese, Anno==myAnno, Giorno_Anno<=myGiorno_Anno)
+#   dataset_polimi[i,]$sp_mese <- sum(as.numeric(temp$Vendite))
+# }
+# 
+# # Somma parziale nell'anno
+# dataset_polimi$sp_anno<-0
+# for (i in 1:nrow(dataset_polimi)){
+#   myProdotto <- dataset_polimi[i,]$Categoria_prodotto
+#   myKey <- dataset_polimi[i,]$Key
+#   myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
+#   myAnno <- dataset_polimi[i,]$Anno
+#   temp <- filter(dataset_polimi, Categoria_prodotto==myProdotto, Key==myKey, Anno==myAnno, Giorno_Anno<=myGiorno_Anno)
+#   dataset_polimi[i,]$sp_anno <- sum(as.numeric(temp$Vendite))
+# }
 
 sample_set <- dataset_polimi[sample(1:nrow(dataset_polimi), 1000), ]
 write.csv(sample_set, file="Modified data/sample_set_polimi.csv")
