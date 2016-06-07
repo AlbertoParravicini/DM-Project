a2507d564a04b2bc33bb6f12c633fe561bf7f80b
Luca Stornaiuolo <storna93@live.it>
2016-06-07 17:29:28 +0200

    reordering and extract seasons

--- src/data preparation.R
+++ src/data preparation.R
@@ -1,32 +1,50 @@
-library(lubridate)
-library(zoo)
-library(dplyr)
-library(xts)
-library(ggplot2)
-library(dplyr)
+library(ggplot2) # visualization
+library(ggthemes) # visualization
+library(dplyr) # dataset_polimi manipulation
+library(lubridate) # dates
+library(rpart) # rpart for imputation
+library(randomForest) # classification algorithm
+library(ranger) # faster version of randomForest
+library(nnet) # For binarization
+library(zoo) # For time series
 
-setwd("~/DM-Project")
-dataset_polimi <- read.csv("~/DM-Project/Original data/dataset_polimi.csv", stringsAsFactors=FALSE)
+# Import data
+#setwd("C:/Users/Storna/Desktop/dm_proj/workspace")
+#dataset_polimi <- read.csv("dataset_polimi.csv", stringsAsFactors=FALSE)
 
-# Estrai il giorno nel mese
+
+# Extract number from string
+dataset_polimi$N_Zona <- strtoi(gsub("Zona_","",dataset_polimi$Zona))
+dataset_polimi$N_Area <- strtoi(gsub("Area_","",dataset_polimi$Area))
+dataset_polimi$N_Sottoarea <- strtoi(gsub("Sottoarea_","",dataset_polimi$Sottoarea))
+dataset_polimi$N_Prodotto <- strtoi(gsub("Prodotto_","",dataset_polimi$Categoria_prodotto))
+
+
+# Reorder table by key
+dataset_polimi <- dataset_polimi[order(dataset_polimi$N_Zona, dataset_polimi$N_Area, dataset_polimi$N_Sottoarea, dataset_polimi$N_Prodotto, dataset_polimi$Data) , ]
+
+
+# Extract information from Data field
 dataset_polimi$Giorno_Mese <- day(dataset_polimi$Data)
-# Estrai il giorno nella settimana
 dataset_polimi$Giorno_Settimana <- wday(dataset_polimi$Data)
-# Estrai il giorno nell'anno
 dataset_polimi$Giorno_Anno <- yday(dataset_polimi$Data)
-# Estrai il mese
 dataset_polimi$Mese <- month(dataset_polimi$Data)
-# Estrai la settimana dell'anno
 dataset_polimi$Settimana_Anno <- week(dataset_polimi$Data)
-# Estrai l'anno
 dataset_polimi$Anno <- year(dataset_polimi$Data)
 
+
+# Extract seasons from Data field
+dataset_polimi$Stagione_Mese <- ifelse(dataset_polimi$Mese <= 2 | dataset_polimi$Mese >= 12, 'inverno',
+                                ifelse(3 <= dataset_polimi$Mese & dataset_polimi$Mese <= 5, 'primavera',
+                                       ifelse(6 <= dataset_polimi$Mese & dataset_polimi$Mese <= 8, 'estate',
+                                              ifelse(9 <= dataset_polimi$Mese & dataset_polimi$Mese <= 11, 'autunno',''))))
 
 # Assegna una chiave in base a zona, area e sottoarea
 dataset_polimi$Key <- paste(dataset_polimi$Zona, paste(dataset_polimi$Area, dataset_polimi$Sottoarea, sep="-"), sep="-")
 
 
 # Nota: la settiman è calcolata a partire dal primo gennaio, senza guardare che giorno sia (e.g. settimana inizia dal giovedi per il 2014)
+
 
 # Somma parziale nella settimana
 dataset_polimi$sp_sett <- 0
