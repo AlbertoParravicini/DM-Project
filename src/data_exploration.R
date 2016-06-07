4e7d5f57a81f49d5538a59de0e08e457851cd68f
Alberto Parravicini <Alberto Parravicini>
2016-06-07 19:53:20 +0200

    Added ts

--- src/data_exploration.R
+++ src/data_exploration.R
@@ -1,6 +1,7 @@
 library(xts)
 library(ggplot2)
 library(dplyr)
+library(PerformanceAnalytics)
 
 setwd("~/DM-Project")
 dataset <- read.csv("~/DM-Project/Modified data/dataset_polimi.csv", stringsAsFactors=FALSE, row.names=NULL)
@@ -36,6 +37,30 @@ nrow(dataset[dataset$prod==1,])
 nrow(dataset[dataset$prod==2,])
 
 summary(dataset)
+
+# Create historical series of product 1.
+data_p1 <- filter(dataset, prod == 1, zona == 1) 
+data_p1 <- data_p1[, c("data", "vendite")]
+data_p1$vendite <- as.numeric(levels(data_p1$vendite))[data_p1$vendite]
+data_p1 <- aggregate(cbind(vendite) ~ data , data = data_p1, FUN = sum)
+
+data_p1$data <- as.Date(as.character(data_p1$data),format="%Y-%m-%d")
+# create xts object
+p1_ts <- xts(data_p1$vendite, data_p1$data)
+
+plot(p1_ts)
+abline(lm(p1_ts~time(p1_ts)), lwd=30, col="red")
+
+
+chart.TimeSeries(p1_ts, legend.loc="bottom", main=" ")
+acf(p1_ts, lag.max = 400)
+pacf(p1_ts, lag.max = 400)
+
+
+
+
+
+
 
 
 
