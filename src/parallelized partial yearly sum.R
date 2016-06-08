3e5af8a4faca0c7c6855d6812c2c55eca32a9438
Simone Ripamonti <simone.ripamonti@mail.polimi.it>
2016-06-08 22:31:51 +0200

    Modifica script parallelo annuale (ancora da lanciare)

--- src/parallelized partial yearly sum.R
+++ src/parallelized partial yearly sum.R
@@ -2,20 +2,20 @@ library(doParallel)
 registerDoParallel()
 
 n<-nrow(dataset_polimi)
+# n<-365
 i<-0
 
 calculate_sp_anno <- function(i) {
 
     myProdotto <- dataset_polimi[i,]$Categoria_prodotto
-    myKey <- dataset_polimi[i,]$Key
-    myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
+    # myKey <- dataset_polimi[i,]$Key
+    # myGiorno_Anno <- dataset_polimi[i,]$Giorno_Anno
     myAnno <- dataset_polimi[i,]$Anno
     
     for (j in 0:i){
       if(i-j==0){break} # stop if beginning of dataset reached
       if(dataset_polimi[i-j,]$Categoria_prodotto != myProdotto 
-         || dataset_polimi[i-j,]$Key != myKey
-         || dataset_polimi[i-j,]$Giorno_Anno > myGiorno_Anno
+
          || dataset_polimi[i-j,]$Anno != myAnno) {break} # stop if reached a "wrong" j
     }
     temp <- 0
@@ -34,6 +34,6 @@ calculate_sp_anno <- function(i) {
 
 # result is a vector that must be bind to the original dataframe
 sp_anno <- foreach(i=1:n, .inorder = TRUE, .combine = 'c') %dopar% calculate_sp_anno(i)
-
+# print()
 
 dataset_polimi <- cbind(dataset_polimi, sp_anno)
