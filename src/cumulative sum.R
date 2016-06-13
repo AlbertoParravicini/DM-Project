b56b6e3cabb8160ed947d113a16aa82d859b0ee8
Simone Ripamonti <simone.ripamonti@mail.polimi.it>
2016-06-13 15:01:13 +0200

    Changed cumsum result to exclude current day in the sum

--- src/cumulative sum.R
+++ src/cumulative sum.R
@@ -1,20 +1,20 @@
 library(dplyr)
 
 dataset <- read.csv("~/DM-Project/dataset_polimi.csv", stringsAsFactors=FALSE, row.names=NULL)
-# need to have run "data preparation.R" on this dataset!
+# need to have run "data preparation.R" & "data preparation2.R" on this dataset!
 
 # =========== CUMULATIVE SUM OF THE YEAR =============================
 v_sp_anno <- vector(mode="numeric", length=0L)
 
-for (k in unique(dataset$Key)){
-  for (p in unique(dataset$Categoria_prodotto)){
-    for (y in unique(dataset$Anno)){
-      v_temp <- vector(mode="numeric", length=0L)
-      cumsum(0)
-      temp <- filter(dataset, Anno==y & Key==k & Categoria_prodotto==p)$Vendite
+for (k in unique(dataset$key)){
+  for (p in unique(dataset$prod)){
+    for (y in unique(dataset$anno)){
+      # v_temp <- vector(mode="numeric", length=0L)
+      temp <- filter(dataset, anno==y & key==k & prod==p)$vendite
       # print(c(k, y, length(temp)))
       if(length(temp)!=0){
-       v_temp <- cumsum(temp) 
+       v_temp <- c(0, cumsum(temp))
+       v_temp <- v_temp[1:(length(v_temp)-1)]
        v_sp_anno <- c(v_sp_anno, v_temp)
       }
     }
@@ -27,16 +27,17 @@ names(dataset)[names(dataset) == 'v_sp_anno'] <- 'sp_anno'
 # ============ CUMULATIVE SUM OF THE MONTH ==============================
 v_sp_mese <- vector(mode="numeric", length=0L)
 
-for (k in unique(dataset$Key)){
-  for ( p in unique(dataset$Categoria_prodotto)){
-    for (y in unique(dataset$Anno)){
-      v_temp <- vector(mode="numeric", length=0L)
-      temp <- filter(dataset, Anno==y & Key==k & Categoria_prodotto==p)
-      for(m in unique(dataset$Mese)){
+for (k in unique(dataset$key)){
+  for ( p in unique(dataset$prod)){
+    for (y in unique(dataset$anno)){
+      # v_temp <- vector(mode="numeric", length=0L)
+      temp <- filter(dataset, anno==y & key==k & prod==p)
+      for(m in unique(dataset$mese)){
         # print(c(k, y, m))
-        temp2 <- filter(temp, Mese==m)$Vendite
+        temp2 <- filter(temp, mese==m)$vendite
         if(length(temp2)!=0){
-          v_temp <- cumsum(temp2) 
+          v_temp <- c(0,cumsum(temp2))
+          v_temp <- v_temp[1:(length(v_temp)-1)]
           v_sp_mese <- c(v_sp_mese, v_temp)
         }
       }
@@ -50,16 +51,17 @@ names(dataset)[names(dataset) == 'v_sp_mese'] <- 'sp_mese'
 # ============ CUMULATIVE SUM OF THE WEEK ==============================
 v_sp_settimana <- vector(mode="numeric", length=0L)
 
-for (k in unique(dataset$Key)){
-  for (p in unique(dataset$Categoria_prodotto)){
-      for (y in unique(dataset$Anno)){
+for (k in unique(dataset$key)){
+  for (p in unique(dataset$prod)){
+      for (y in unique(dataset$anno)){
         v_temp <- vector(mode="numeric", length=0L)
-        temp <- filter(dataset, Anno==y & Key==k & Categoria_prodotto==p)
-        for(w in unique(dataset$Settimana_Anno)){
+        temp <- filter(dataset, anno==y & key==k & prod==p)
+        for(w in unique(dataset$settimana_anno)){
           # print(c(k, y, w))
-          temp2 <- filter(temp, Settimana_Anno==w)$Vendite
+          temp2 <- filter(temp, settimana_anno==w)$vendite
           if(length(temp2)!=0){
-            v_temp <- cumsum(temp2) 
+            v_temp <- c(0,cumsum(temp2))
+            v_temp <- v_temp[1:(length(v_temp)-1)]
             v_sp_settimana <- c(v_sp_settimana, v_temp)
           }
       }
@@ -73,6 +75,6 @@ names(dataset)[names(dataset) == 'v_sp_settimana'] <- 'sp_settimana'
 
 # ============  WRITEBACK
 
-write.csv(dataset, file="Modified data/dataset_polimi_with_cumsum.csv")
+write.csv(dataset, file="Modified data/dataset_polimi_final.csv")
 
 
