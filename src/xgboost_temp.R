ce13e906f7dbab96bd104617861871a430f018ed
Simone Ripamonti <simone.ripamonti@mail.polimi.it>
2016-06-20 00:16:22 +0200

    Good xgboost (sse=1.90, MAPE=0.27, MAXAPE=4.03)

--- src/xgboost_temp.R
+++ src/xgboost_temp.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(xgboost)
 library(ggthemes) # visualization
 source("src/scoring functions.R")
+source("src/sarima.R")
 library(stringr)
 library(xts)
 library(ggplot2)
@@ -54,6 +55,42 @@ if (class(data_test$vendite) == "factor") {
 }
 
 
+# ########### BEGIN VENDITE GIORNALIERE PRODOTTO #########################
+# 
+# # get sarima predictions for the product days
+# prod_1_daily <- pred_test_regressors(prediction_start = (max(dataset$data) - prediction_length +1),
+#                                      prediction_length=prediction_length, num_prod=1)
+# prod_2_daily <- pred_test_regressors(prediction_start = (max(dataset$data) - prediction_length +1),
+#                                      prediction_length=prediction_length, num_prod=2)
+# 
+# data_test <-data_test[, !(names(data_test) %in% c("vendite_giorn_prod", "vendite_giorn_tot"))]
+# data_test_p_1 <- filter(data_test, prod == 1)
+# data_test_p_1$data <- as.Date(data_test_p_1$data, format = "%Y-%m-%d")
+# data_test_p_2 <- filter(data_test, prod == 2)
+# data_test_p_2$data <- as.Date(data_test_p_2$data, format = "%Y-%m-%d")
+# 
+# # prod_1_daily$data <- as.numeric(as.character(prod_1_daily$data))
+# # prod_1_daily$data <- as.Date("1970-01-01", format="%Y-%m-%d") + prod_1_daily$data
+# 
+# # total_table <- merge(data_test, prod_1_daily, by = c("prod", "data"), all.x = T)
+# 
+# prod_1_d <- data.frame(as.vector(prod_1_daily), as.Date(index(prod_1_daily)))
+# names(prod_1_d)[names(prod_1_d)=="as.Date.index.prod_1_daily.."] <- "data"
+# names(prod_1_d)[names(prod_1_d)=="as.vector.prod_1_daily."] <- "vendite_giorn_prod"
+# prod_2_d <- data.frame(as.vector(prod_2_daily), as.Date(index(prod_2_daily)))
+# names(prod_2_d)[names(prod_2_d)=="as.Date.index.prod_2_daily.."] <- "data"
+# names(prod_2_d)[names(prod_2_d)=="as.vector.prod_2_daily."] <- "vendite_giorn_prod"
+# 
+# 
+# data_test_p_1 <- left_join(data_test_p_1, prod_1_d)
+# # names(data_test_p_1)[names(data_test_p_1)=="prod_1_daily"] <- "vendite_giorn_prod"
+# data_test_p_2 <- left_join(data_test_p_2, prod_2_d)
+# # names(data_test_p_2)[names(data_test_p_2)=="prod_2_daily"] <- "vendite_giorn_prod"
+# data_test <- rbind(data_test_p_1, data_test_p_2)
+# 
+# ########## END VENDITE GIORNALIERE PRODOTTO ##################
+
+
 # ========================================================================================
 # ========================================================================================
 # ============================XGBoost model builder ======================================
@@ -85,7 +122,7 @@ xg_single <- function(n_rounds=45, details=F){
   
   # build model
   xgb_model <- xgb.train(data=xg_train, nrounds = n_rounds, nthread = 4, 
-                         watchlist=watchlist)
+                         watchlist=watchlist, eta = 0.095)
   xgb_pred <- predict(xgb_model, xg_test)
 
   # get some scoring
@@ -111,5 +148,34 @@ xg_single <- function(n_rounds=45, details=F){
 
 }
 
-xgb_pred <- xg_single(n_rounds=167,details=T)
+xgb_pred <- xg_single(n_rounds=650,details=T)
 
+# eta 0.2 rounds = 250
+# SSE:  2.009147 
+# MAPE:  0.276284 
+# MAX APE:  3.992809 
+
+# eta 0.1 nrounds=667
+# SSE:  1.904172 
+# MAPE:  0.2708243 
+# MAX APE:  4.035991 
+
+# eta 0.095 nrounds 700
+# SSE:  1.923633 
+# MAPE:  0.2686256 
+# MAX APE:  4.051457 
+
+# eta 0.09 nrounds = 780
+# SSE:  1.982959 
+# MAPE:  0.2767076 
+# MAX APE:  4.065648 
+
+# eta 0.075 nrounds =780
+# SSE:  1.993004 
+# MAPE:  0.2746128 
+# MAX APE:  4.059773 
+
+# eta 0.05 nrounds 1123
+# SSE:  1.974349 
+# MAPE:  0.2726675 
+# MAX APE:  4.037817 
